69f84b1782d0134ac61e46bddbe0d8762cba8148
Jay Chen <gcobs135102@gmail.com>
2019-09-26 11:43:23 +0800

    chore: add packages core, react
    TODO: commonjs and esm bundle

---
+++ packages/core/src/index.ts
@@ -0,0 +1,5 @@
+export class MonoModule {
+	constructor(
+		public name: string
+	) { }
+}
---
+++ packages/react/src/index.ts
@@ -0,0 +1,8 @@
+import { createContext, useContext } from 'react';
+import { MonoModule } from '@jay-mono-ts/core';
+
+export const MonoModuleContext = createContext<MonoModule>({} as MonoModule);
+
+export function useMonoModule() {
+	return useContext(MonoModuleContext);
+}
